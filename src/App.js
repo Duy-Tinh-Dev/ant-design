418b1bb0495485ea9267953790c430e82cdf02f3
DuyTinh <93391369+Tinhturtle@users.noreply.github.com>
2022-09-13 00:42:00 +0700

    create ant design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Layout } from "antd";
+import Header from "./components/header";
+import SideBar from "./components/sideBar";
+import Content from "./components/content";
+// const { Header, Footer, Sider, Content } = Layout;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header />
+        <Layout>
+          <Content />
+          {/* <Sider>Sider</Sider> */}
+        </Layout>
+        {/* <Footer>Footer</Footer> */}
+      </Layout>
     </div>
   );
 }
---
+++ src/components/content/index.jsx
@@ -0,0 +1,5 @@
+function Content() {
+  return <h1>This is content</h1>;
+}
+
+export default Content;
